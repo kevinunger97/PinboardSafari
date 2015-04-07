02259fe380f080f6f450c998b402e6b678484645
mariofink <kontakt@mariofink.de>
2015-04-07 20:59:48 +0200

    using separate tab instead of popover to avoid some trouble for now

--- PinboardSafari.safariextension/global.js
+++ PinboardSafari.safariextension/global.js
@@ -2,7 +2,12 @@ var _console = safari.extension.globalPage.contentWindow.console;
 
 function performCommand (command) {
   if (command.command === "btn_pinit_clicked") {
-    util.showPopover("popover-add-bookmarks", "btn_pinit");
+    //util.showPopover("popover-add-bookmarks", "btn_pinit");
+    var toBookmark = encodeURIComponent(safari.application.activeBrowserWindow.activeTab.url);
+    var title = safari.application.activeBrowserWindow.activeTab.title;
+    var url = "https://pinboard.in/add?url=" + toBookmark + "&title=" + encodeURIComponent(title);
+    var browserWindow = safari.application.activeBrowserWindow;
+    browserWindow.openTab().url = url;
   }
 }
 
